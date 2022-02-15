614c307e2c844dfba9bb40d72dc8d68a6975d3e2
jackbottaro <mail.giacomobottaro@gmail.com>
2022-02-15 11:48:13 +0100

    added header as component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
@@ -24,5 +23,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body{
+  background-color: #333f48;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+<header>
+  <div class="col">
+    <img src="https://brand.netflix.com/static/assets/icons/netflix_logo.svg" alt="">
+  </div>
+  <div class="col search-area">
+    <input type="text">
+    <button>CERCA</button>
+  </div>
+</header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header{
+  height: 75px;
+  background-color: #221f1f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+img{
+  width: 100px;
+  height: 100%;
+  margin-left: 50px;
+  animation: pulse 2s  infinite ;
+  @keyframes pulse {
+	0% {
+		transform: scale(1);
+	}
+
+  50% {
+		transform: scale(1.1);
+	}
+
+	100% {
+		transform: scale(1);
+	}
+  }
+}
+
+.search-area{
+  margin-right: 50px;
+}
+
+input{
+  margin: 15px;
+}
+
+button{
+  background-color: #d81f26;
+  border: 0;
+  color: white;
+  font-weight: bold;
+  padding: 2px 11px;
+  border-radius: 5px;
+}
+
+</style>
